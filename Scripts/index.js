ba16b10a25b3d61425a36c7fcb5f9caa8fec828c
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-03-06 12:28:59 -0800

    Update index

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,11 +1,16 @@
 "use strict";
 $(document).ready(() => {
+    //cookie
+        //if theres a cookie
+            //get the array and pass it to the API
+        // if no cookie
+            //create one
         var someStocks = ["APL", "AMZN", "MSFT", "TSLA"];
 
         $.each(someStocks, function (index, point) {
             $('#favorites').append(
                 '<div class = "stock_box">' +
-                    '<h1>'+point+'</h1>' +
+                    '<h2>'+point+'</h2>' +
                     '<h2>$12.34</h2>' +
                 '</div>'
             );
