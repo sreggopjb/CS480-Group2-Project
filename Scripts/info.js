1f8448b554730b09d4c9ca8b5bb474982fac5b49
sreggopjb <100386775+sreggopjb@users.noreply.github.com>
2023-03-07 14:12:01 -0800

    Graph loaded in. Still need to fix formatting and figure out how to pass stock symbols to it.

--- Scripts/info.js
+++ Scripts/info.js
@@ -3,49 +3,21 @@
 (function () {
   'use strict'
   feather.replace({ 'aria-hidden': 'true' })
-  // Graph
-  var ctx = document.getElementById('myChart')
   //Placeholder data
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
+  var myChart = new TradingView.widget(
+        {
+        "autosize": true,
+        "symbol": "NASDAQ:AAPL",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "myChart"
+        }
+    );
 })()
+
