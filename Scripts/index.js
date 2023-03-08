bef01d74d3db72fdfdb345fb4c02690b9e2b20c2
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-03-08 12:31:09 -0800

    Trying to pass a string

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,38 +2,39 @@
 const someStocks = ["AAPL", "AMZN", "MSFT", "TSLA"];
 $(document).ready(() => {
 //cookie
-        //if theres a cookie
-            //get the array and pass it to the API
-        // if no cookie
-            //create one
+  //if theres a cookie
+  //get the array and pass it to the API
+  // if no cookie
+  //create one
   const apiKey = "NMBJV9I1JXOWWEZ6";
 
 
   for (let i = 0; i < someStocks.length; i++) {
+    let name = "\""+someStocks[i]+"\"";
     $.getJSON(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${someStocks[i]}&outputsize=full&apikey=${apiKey}`
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${someStocks[i]}&outputsize=full&apikey=${apiKey}`
     ).then((data) => {
       const stockInfo = data["Time Series (Daily)"][Object.keys(data["Time Series (Daily)"])[0]];
       const stockPrice = parseFloat(stockInfo["4. close"]).toFixed(2);
-
       $('#favorites').append(`
-        <a onclick="showStock(someStocks[i])">
+ 
         <div class="stock_box">
            <button>Delete</button>
-           
           <h2>${someStocks[i]}</h2>
           <h2 class="stock_price">$${stockPrice}</h2>
-        </div>
-        </a>
+          <input id="more info" type="button" value="more info" onclick="showStock(${name})" />
+        </div>x
       `);
     });
   }
 });
 
-
+function showStock(stock = "aaple"){
+  console.log(stock);
+  //localStorage.setItem("stock", stock);
+}
 function addStock(){
   let popup = document.getElementById("add_popup");
   document.location.reload();
 
 }
-
