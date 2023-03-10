1f93751ee49eb85120aaecdebb4582fe32a9edc4
sreggopjb <100386775+sreggopjb@users.noreply.github.com>
2023-03-09 16:27:17 -0800

    Added click event listeners for stock_box and delete_button elements to fix bug where clicking the delete button would take you to the info.html page instead of just running the delete function.

--- Scripts/index.js
+++ Scripts/index.js
@@ -26,15 +26,28 @@ $(document).ready(() => {
         const stockInfo = data["Time Series (Daily)"][Object.keys(data["Time Series (Daily)"])[0]];
         const stockPrice = parseFloat(stockInfo["4. close"]).toFixed(2);
         $('#favorites').append(`
-          <div onclick="showStock('${stock}')" class="stock_box" style="cursor: pointer;">
+          <div class="stock_box" style="cursor: pointer;">
               <div>
-                  <a href ='#' onclick="del('${stock}')"><i class="fa fa-trash stock_buttons delete_button"></i></a>
+                  <a href ='#'><i class="fa fa-trash stock_buttons delete_button"></i></a>
                   <a><i class="fa fa-refresh stock_buttons change_button"></i></a>
               </div>
             <h2>${stock}</h2>
             <h2 class="stock_price">$${stockPrice}</h2>
           </div>
       `);
+        // Click event listener for stock_box element
+        $('.stock_box').on('click', function(event) {
+          //Stops the click event from bubbling up to underlying elements
+          event.stopPropagation();
+          showStock(stock);
+        });
+
+        // Click event listener for delete_button element
+        $('.delete_button').on('click', function(event) {
+          //Stops the click event from bubbling up to underlying elements
+          event.stopPropagation();
+          del(stock);
+        });
       });
     });
   });
