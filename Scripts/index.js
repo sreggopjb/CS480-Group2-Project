849a8acc12c16a223d4b30cfaf0cfe4e8cbb48ca
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-03-14 14:43:20 -0700

    fixed cookie

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,58 +1,51 @@
 "use strict";
-import Cookies from '/path/to/js.cookie.mjs'
-let favorites;
+let favorites = new Array();
 $(document).ready(() => {
-  if(!checkCookie()){
-    Cookies.set('favorites', favorites, {expires: 3});
-  }
-  favorites = Cookies.get('favorites');
+
+  if(!checkCookie()) createCookie(favorites);
+  favorites = getCookie();
+
+
   const apiKey = "NMBJV9I1JXOWWEZ6";
 
-
-
-  // Check if there is a cookie and retrieve the array if it exists
-  //const cookieExists = checkCookie();
-  let favorites = ["TSLA", "AAPL", "AMZN", "MSFT"];
-  /*if (cookieExists) {
-    favorites = JSON.parse(getCookie("favorites"));
-  }*/
-
     // Loop through the favorites array and make API requests for each stock
-    favorites.forEach((stock) => {
-      $.getJSON(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock}&outputsize=full&apikey=${apiKey}`
-      ).then((data) => {
-        name = stock;
-        const stockInfo = data["Time Series (Daily)"][Object.keys(data["Time Series (Daily)"])[0]];
-        const stockPrice = parseFloat(stockInfo["4. close"]).toFixed(2);
-        $('#favorites').append(`
-          <div class="stock_box" style="cursor: pointer;">
-              <div>
-                  <a href ='#' ><i class="fa fa-trash stock_buttons delete_button"></i></a>
-                  <a><i class="fa fa-refresh stock_buttons change_button"></i></a>
-              </div>
-            <h2>${stock}</h2>
-            <h2 class="stock_price">$${stockPrice}</h2>
-          </div>
-      `);
-        // Click event listener for stock_box element
-        $('.stock_box').on('click', function(event) {
-          //Stops the click event from bubbling up to underlying elements
-          event.stopPropagation();
-          showStock(stock);
-        });
+  if(favorites!= null && favorites.length > 0){
+      favorites.forEach((stock) => {
+        $.getJSON(
+            `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock}&outputsize=full&apikey=${apiKey}`
+        ).then((data) => {
+          name = stock;
+          const stockInfo = data["Time Series (Daily)"][Object.keys(data["Time Series (Daily)"])[0]];
+          const stockPrice = parseFloat(stockInfo["4. close"]).toFixed(2);
+          $('#favorites').append(`
+            <div class="stock_box" style="cursor: pointer;">
+                <div>
+                    <a href ='#' ><i class="fa fa-trash stock_buttons delete_button"></i></a>
+                    <a><i class="fa fa-refresh stock_buttons change_button"></i></a>
+                </div>
+              <h2>${stock}</h2>
+              <h2 class="stock_price">$${stockPrice}</h2>
+            </div>
+        `);
+          // Click event listener for stock_box element
+          $('.stock_box').on('click', function(event) {
+            //Stops the click event from bubbling up to underlying elements
+            event.stopPropagation();
+            showStock(stock);
+          });
 
-        // Click event listener for delete_button element
-        $('.delete_button').on('click', function(event) {
-          //Stops the click event from bubbling up to underlying elements
-          event.stopPropagation();
-          del(stock);
+          // Click event listener for delete_button element
+          $('.delete_button').on('click', function(event) {
+            //Stops the click event from bubbling up to underlying elements
+            event.stopPropagation();
+            del(stock);
+          });
         });
       });
-    });
-  });
 
-  // Function to add a stock to favorites array and create/update cookie
+  }
+    });
+
 
 function showStock(stock = "aaple"){
   console.log(stock);
@@ -60,6 +53,7 @@ function showStock(stock = "aaple"){
   window.location.assign("info.html");
 }
 function editMode() {
+  console.log("EditMode");
   document.getElementById("editMode").style.visibility = "visible";
   const temp =document.getElementsByClassName("stock_buttons");
   for(let i= 0; i< temp.length; i++){
@@ -68,12 +62,16 @@ function editMode() {
   let button = document.getElementById("edit_button");
   button.innerText = "Back";
   button.setAttribute("onclick", 'toggleEdit()');
+
 }
 function toggleEdit(){
   document.getElementById("editMode").style.visibility = "hidden";
-  const temp =document.getElementsByClassName("stock_buttons");
-  for(let i= 0; i< temp.length; i++){
-    temp[i].style.visibility = "hidden";
+  if(favorites.length >0){
+
+    const temp =document.getElementsByClassName("stock_buttons");
+    for(let i= 0; i< temp.length; i++){
+      temp[i].style.visibility = "hidden";
+    }
   }
   let button = document.getElementById("edit_button");
   button.innerText = "Edit";
@@ -82,9 +80,11 @@ function toggleEdit(){
 
 }
   // Show add stock popup
+function showAdd(){
+  document.getElementById("add_form").style.visibility=  'visible';
 
+}
 
-  // Hide add stock popup
 
 
   // Delete stock from favorites array and update cookie
@@ -92,23 +92,25 @@ function del(stock){
   console.log(stock);
   let x = favorites.indexOf(stock);
   favorites.splice(x, 1);
-  Cookies.set("favorites", JSON.stringify(favorites));
+  createCookie(favorites);
   window.location.reload();
 
 
 }
-function showAdd(){
-  document.getElementById("add_form").style.visibility=  'visible';
 
-}
 
-function add_stock(stock){
-  if(favorites.indexOf(stock) !=-1){
-    document.getElementById("error").innerText = "That Stock already exists in the dashboard";
-    return;
+function add_stock(){
+  let x = document.getElementById("stock_name").value;
+  if(favorites == null){
+    favorites = [x];
+    console.log(favorites);
   }
-  favorites.push(stock);
-  Cookies.set("favorites", JSON.stringify(favorites));
+  else{
+    favorites.push(x);
+    console.log(favorites);
+  }
+
+  createCookie(favorites);
   window.location.reload();
 }
   // Function to check if a cookie exists
@@ -117,22 +119,36 @@ function add_stock(stock){
     return document.cookie.indexOf("favorites") !== -1;
   }
 
-  // Function to set a cookie
-/*
-// Function to get the value of a cookie
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(nameEQ) == 0) {
-      return c.substring(nameEQ.length, c.length);
-    }
+  function createCookie(favorites){
+    let date = new Date();
+    date.setTime(date.getTime() + (2*24 *60*60*2000));
+    let expires  = "expires=" + date.toUTCString();
+    let arr = JSON.stringify(favorites);
+    document.cookie = "favorites ="+arr+";" + expires + ";path =/";
+
+}
+function getCookie() {
+  let cookieArray = new Array();
+  let result = new Array();
+
+  //Get cookie
+  let cookieValue = document.cookie;
+
+  //Divide the cookie into an array and convert them to JSON
+  if(cookieValue){
+    cookieArray = cookieValue.split(';');
+
+    cookieArray.forEach(data => {
+      data = data.split('=');
+
+      //data[0]: Cookie name
+      //data[1]: Cookie value
+
+      console.log(data[0]+ ' ' + data[1]);
+      result = JSON.parse(data[1]);
+
+    });
   }
-  return null;
+  return result;
 }
 
-*/
