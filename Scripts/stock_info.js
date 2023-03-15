61226a15ea9b392539e88000b6f8274b15e6e343
Racheljoans <94077195+Racheljoans@users.noreply.github.com>
2023-03-15 10:35:10 -0700

    api call for beta pe and eps

--- Scripts/stock_info.js
+++ Scripts/stock_info.js
@@ -10,6 +10,15 @@ $(document).ready(()=>{
         const stockInfo = data["Time Series (Daily)"][Object.keys(data["Time Series (Daily)"])[0]];
         open = parseFloat(stockInfo["1. open"]).toFixed(2);
         close = parseFloat(stockInfo["4. close"]).toFixed(2);
+
+    });
+    $.getJSON(
+        `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stock}&apikey=${apiKey}`
+    ).then((data) => {
+        beta = parseFloat(data["Beta"]).toFixed(2);
+        pe = parseFloat(data["PERatio"]).toFixed(2);
+        eps = parseFloat(data["EPS"]).toFixed(2);
+        // do something with beta, pe, and eps
     });
 
     $("#stock_info").append(`
@@ -21,6 +30,18 @@ $(document).ready(()=>{
             <tr>
                 <td>Close</td>
                 <td>${close}</td>
+            </tr>
+            <tr>
+                <td>Beta</td>
+                <td>{beta}</td>
+            </tr>
+            <tr>
+                <td>Price to Earning</td>
+                <td>{pe}</td>
+            </tr>
+            <tr>
+                 <td>Earnings Per Share </td>
+                 <td>{eps}</td>
             </tr>
         </table>
         
